f8a74961ed11a6e014afdf9b5d95c80467ec5335
robert_r <remi.robert@epitech.eu>
2014-05-13 10:47:25 +0200

    add action implementation

---
+++ server/action/object_action.c
@@ -0,0 +1,28 @@
+#include "../client.h"
+
+void			action_inventary(t_server *server,
+					 t_client *current_client)
+{
+  (void)server;
+  (void)current_client;
+}
+
+void			action_take_object(t_server *server,
+					   t_client *current_client)
+{
+  (void)server;
+  (void)current_client;
+}
+
+void		        action_drop_object(t_server *server,
+					   t_client *current_client)
+{
+  (void)server;
+  (void)current_client;
+}
+
+void			action_see(t_server *server, t_client *current_client)
+{
+  (void)server;
+  (void)current_client;
+}
