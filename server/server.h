1509bd7b984de780dacd65f440b10e4723d30440
Remi Hillairet <remihillairet@msn.com>
2014-05-13 16:58:42 +0200

    Commit before merge

--- server/server.h
+++ server/server.h
@@ -1,5 +1,15 @@
-#ifndef SERVER_H
-# define SERVER_H
+/*
+** server.h for zappy in /Users/remihillairet/Epitech/Github/Zappy/server
+**
+** Made by remi hillairet
+** Login   <remihillairet@epitech.net>
+**
+** Started on  Tue May  13 10:53:07 2014 remi hillairet
+** Last update Tue May  13 10:53:07 2014 remi hillairet
+*/
+
+#ifndef SERVER_H_
+# define SERVER_H_
 
 # include "client.h"
 # include "list.h"
@@ -9,4 +19,4 @@ typedef struct		s_server
   t_list		*clients;
 }			t_server;
 
-#endif // SERVER
+#endif /* !SERVER_H_ */
--- server/t_list.h
+++ server/t_list.h
@@ -1,5 +1,15 @@
-#ifndef LIST_H
-# define LIST_H
+/*
+** t_list.h for zappy in /Users/remihillairet/Epitech/Github/Zappy/server
+**
+** Made by remi hillairet
+** Login   <remihillairet@epitech.net>
+**
+** Started on  Tue May  13 10:53:47 2014 remi hillairet
+** Last update Tue May  13 10:53:47 2014 remi hillairet
+*/
+
+#ifndef LIST_H_
+# define LIST_H_
 
 typedef struct		s_list
 {
@@ -7,4 +17,4 @@ typedef struct		s_list
   struct s_list		*next;
 }			t_list;
 
-#endif // LIST
+#endif /* !LIST_H_ */
