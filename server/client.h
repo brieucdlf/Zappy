0930ce184bc4389838b16dca4f7e3fd491ad17f3
Remi Hillairet <remihillairet@msn.com>
2014-05-13 10:48:09 +0200

    Add headers in file

--- server/client.h
+++ server/client.h
@@ -1,5 +1,15 @@
-#ifndef CLIENT_H
-# define CLIENT_H
+/*
+** client.h for zappy in /Users/remihillairet/Epitech/Github/Zappy/server
+**
+** Made by remi hillairet
+** Login   <remihillairet@epitech.net>
+**
+** Started on  Tue May  13 10:43:27 2014 remi hillairet
+** Last update Tue May  13 10:43:27 2014 remi hillairet
+*/
+
+#ifndef CLIENT_H_
+# define CLIENT_H_
 
 typedef enum
   {
@@ -18,16 +28,16 @@ typedef enum
   }action;
 
 typedef struct		s_action
-{
-  char			current_action;
-  action		current_cycle;
-}			t_action;
+  {
+    char		current_action;
+    action		current_cycle;
+  }			t_action;
 
 typedef struct		s_client
-{
-  int			level;
-  int			items[6];
-  t_action		current_action;
-}			t_client;
+  {
+    int			level;
+    int			items[6];
+    t_action		current_action;
+  }			t_client;
 
-#endif /* CLIENT */
+#endif /* !CLIENT_H_ */
--- server/main.c
+++ server/main.c
@@ -1,3 +1,13 @@
+/*
+** main.c for zappy in /Users/remihillairet/Epitech/Github/Zappy/server
+**
+** Made by remi hillairet
+** Login   <remihillairet@epitech.net>
+**
+** Started on  Tue May  13 10:45:33 2014 remi hillairet
+** Last update Tue May  13 10:45:33 2014 remi hillairet
+*/
+
 #include "client.h"
 
 int			main()
