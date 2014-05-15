fc9c2e488dd915dba08afd783c4232170cd2a86f
robert_r <remi.robert@epitech.eu>
2014-05-15 13:36:04 +0200

    remove warninig

--- server/src/run_server.c
+++ server/src/run_server.c
@@ -1,9 +1,9 @@
 /*
 ** run_server.c for zappy in /home/delafo_b/Projets/Zappy/server/src
-** 
+**
 ** Made by Brieuc
 ** Login   <delafo_b@epitech.net>
-** 
+**
 ** Started on  Thu May 15 13:21:25 2014 Brieuc
 ** Last update Thu May 15 13:33:24 2014 Brieuc
 */
@@ -14,10 +14,11 @@ int                     g_server_run = 1;
 
 void			server_loop(t_server *server)
 {
-  /* Ici select */
+  (void)server;
   while (g_server_run)
     {
       if (g_server_run == 0)
 	return ;
+      /* Ici select */
     }
 }
