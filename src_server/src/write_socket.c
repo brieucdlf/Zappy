b8e647f5ca42f6d3ec19b3917d13a73add09c71b
Rémi Robert <remi.robert@epitech.eu>
2014-06-09 11:11:09 +0200

    write socket server

--- src_server/src/write_socket.c
+++ src_server/src/write_socket.c
@@ -12,7 +12,13 @@ int			map_check_write_client(t_list *current_client,
   client = (t_client *)current_client->data;
   if (FD_ISSET(client->fd_socket, &(server->writefd)))
     {
-      printf("write ok client ok\n");
+      if ((write(client->fd_socket, "OK\n", 3)) <= 0)
+	{
+	  printf("\033[31mDeconnection client [%d]\033[00m\n", client->fd_socket);
+	  deconnection_client(server, client);
+	  printf("new fd max : %d\n", server->fd_max);
+	  return (0);
+	}
     }
   return (1);
 }
