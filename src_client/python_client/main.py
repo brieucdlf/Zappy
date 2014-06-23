b50bb0cd8cd6ec0ec724b21e88eccd80b18cb397
Rémi Robert <remi.robert@epitech.eu>
2014-06-23 09:42:10 +0200

    complete connexion server

--- src_client/python_client/main.py
+++ src_client/python_client/main.py
@@ -1,13 +1,19 @@
 import sys
 import os
 from socket_server import SocketServer
+from argument import parse_argument
 
-def main(argv):
-    so = SocketServer("127.0.0.1", 65510)
+def connect_socket(argument):
+    so = SocketServer(argument["ip"], argument["port"])
     if so.connect_server() == True:
         so.send_request("salut\n")
     else:
-        print "Error creation socket"
+        print("Error creation socket")
+
+def main(argv):
+    config_server = {}
+    config_server = parse_argument(argv).__dict__
+    connect_socket(config_server)
     
 if __name__ == "__main__":
     main(sys.argv)
