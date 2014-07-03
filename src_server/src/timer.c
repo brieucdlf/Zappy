f51760d3289a94bc26657d4aa13e05e50cc3fc4f
remirobert <remi.robert@epitech.eu>
2014-07-03 22:24:11 +0200

    add check for task with argument

--- src_server/src/timer.c
+++ src_server/src/timer.c
@@ -26,7 +26,9 @@ void			check_timer_client(t_list *item, void *server)
 	  tv.tv_usec >= ((t_task *)current_item->data)->timer.tv_usec)
 	{
 	  ((t_task *)current_item->data)->is_over = 1;
-	  ((t_task *)current_item->data)->function(server, current_client, NULL);
+	  ((t_task *)current_item->data)->function(server, current_client,
+						   ((t_task *)current_item->
+						    data)->argument);
 	  list_remove_with_data(&current_client->tasks, NULL,
 				match_delete_task_function);
 	  return ;
