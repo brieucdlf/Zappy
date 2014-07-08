4a1b5a6d2a78f1bc3bba246ee4e8c22e04a84f7b
remirobert <remi.robert@epitech.eu>
2014-07-08 14:54:55 +0200

    add voir task

--- src_server/src/task/command_voir.c
+++ src_server/src/task/command_voir.c
@@ -1,40 +1,110 @@
 #include "server.h"
 
-void			loop_items_on_case(t_list *current_item, void *arg)
+void			make_request(t_client *client,
+				     char *add_command)
+{
+  static char		*command = NULL;
+  static int		index = 0;
+
+  if (client == NULL && add_command == NULL)
+    {
+      index = 0;
+      if ((command = malloc(10240)) != NULL)
+	memset(command, 0, 10240);
+      return ;
+    }
+  if (client != NULL && command != NULL)
+    {
+      printf("command request : %s\n", command);
+      create_new_write_task(client, command);
+      free(command);
+      command = NULL;
+      return ;
+    }
+  if (add_command != NULL && command != NULL)
+    {
+      strncpy(&command[index], add_command,
+	      (strlen(add_command) % (10240 - index)));
+      index += strlen(add_command) % 10240;
+    }
+}
+
+void			loop_items_on_case(t_list *current_item,
+					   void *marker_space)
 {
   t_item		*item;
-  const char		*name_item[7] = {"linemate", "deraumere", "sibur",
-					 "mendiane", "phiras", "thystame",
-					 "food"};
+  const char		*name_item[7] = {" linemate", " deraumere", " sibur",
+					 " mendiane", " phiras", " thystame",
+					 " food"};
+  static int		pass = 0;
 
-  (void)arg;
   if ((item = current_item->data) == NULL ||
       item->type - 1 < 0 || item->type - 1 >= 7)
     return ;
-  printf("current item : %s\n", name_item[item->type - 1]);
+  if (*(int *)marker_space == 1 && pass == 0)
+    {
+      make_request(NULL, (char *)&name_item[item->type - 1][1]);
+      pass = 1;
+    }
+  else if (*(int *)marker_space > 1)
+    {
+      pass = 0;
+      make_request(NULL, (char *)name_item[item->type - 1]);
+    }
 }
+
+void			check_player_at_position(t_server *server,
+						 int position_x, int position_y)
+{
+  t_list		*current_item;
+
+  if ((current_item = server->clients) == NULL)
+    return ;
+  while (current_item != NULL)
+    {
+      if (((t_client *)current_item->data)->direction.position_x == position_x &&
+	  ((t_client *)current_item->data)->direction.position_y == position_y)
+	make_request(NULL, server->
+		     param_server.teams_names[((t_client *)current_item->
+					       data)->id_team]);
+      current_item = current_item->next;
+    }
+}
+
 
 void			check_line(t_server *server, t_client *client,
 				   int current_level, int *direction_position)
 {
   int			index_position;
+  int			marker_position;
 
   direction_position[0] *= current_level + 1;
   direction_position[1] *= current_level;
   for (index_position = 0; index_position < current_level * 2 + 1;
        index_position++)
     {
-      printf("Current position voir : %d %d\n",
-	     client->direction.position_x + direction_position[0] - (current_level / 2 + 1) + index_position,
-	     client->direction.position_y + direction_position[1]);
-
-      if (client->direction.position_x + direction_position[0] - (current_level / 2 + 1) + index_position >= 0 &&
-	  client->direction.position_x + direction_position[0] - (current_level / 2 + 1) + index_position < server->map.width &&
+      marker_position = index_position + current_level;
+      if (client->direction.position_x + direction_position[0] -
+	  (current_level / 2 + 1) + index_position >= 0 &&
+	  client->direction.position_x + direction_position[0] -
+	  (current_level / 2 + 1) + index_position < server->map.width &&
 	  client->direction.position_y + direction_position[1] >= 0 &&
-	  client->direction.position_y + direction_position[1] < server->map.height)
-	map_list(server->map.map[client->direction.position_y + direction_position[1]]
-		 [client->direction.position_x + direction_position[0] - (current_level / 2 + 1) + index_position], loop_items_on_case, NULL);
-
+	  client->direction.position_y + direction_position[1] <
+	  server->map.height)
+	{
+	  map_list(server->map.map[client->direction.position_y +
+				   direction_position[1]]
+		   [client->direction.position_x + direction_position[0] -
+		    (current_level / 2 + 1) + index_position],
+		   loop_items_on_case, (void *)&marker_position);
+	  check_player_at_position(server, client->direction.position_x +
+				   direction_position[0] -
+				   (current_level / 2 + 1) + index_position,
+				   client->direction.position_y +
+				   direction_position[1]);
+	}
+      if (index_position + 1 < current_level * 2 + 1)
+	make_request(NULL, ",");
     }
 }
 
@@ -59,6 +129,7 @@ void			voir_task_function(t_server *server,
   int			index_level_client;
 
   (void)arg;
+  make_request(NULL, NULL);
   printf("voir function\n");
   printf("client : %d %d => level : %d\n", client->direction.position_x,
 	 client->direction.position_y, client->level);
@@ -66,10 +137,10 @@ void			voir_task_function(t_server *server,
   direction_position[1] = 0;
   direction_see(client, &direction_position[0], &direction_position[1]);
   printf("direction debug = %d %d\n", direction_position[0], direction_position[1]);
+  make_request(NULL, "{");
   for (index_level_client = 1; index_level_client <= client->level;
        index_level_client++)
-    {
-      check_line(server, client, index_level_client, direction_position);
-    }
-  create_new_write_task(client, "OK\n");
+    check_line(server, client, index_level_client, direction_position);
+  make_request(NULL, "}\n");
+  make_request(client, NULL);
 }
