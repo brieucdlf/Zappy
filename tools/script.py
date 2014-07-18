5e3766ddfac03c2421eda0fab3ce7199a70bd5d5
Brieuc <brieuc.delafouchardiere@epitech.eu>
2014-07-18 10:12:44 +0200

    Delete script.py

--- tools/script.py
+++
@@ -1,9 +0,0 @@
-import os
-import threading
-
-def execute_client():
-    os.system("python ../src_client/client_infinity/main.py")
-
-for i in range(1, 50):
-    t = threading.Thread(target=execute_client)
-    t.start()
